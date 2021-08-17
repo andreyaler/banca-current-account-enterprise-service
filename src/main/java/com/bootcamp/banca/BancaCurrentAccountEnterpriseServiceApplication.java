13f64cba85050f98ebdbfa211300bded900edb8e
Andre Robinson Yataco Lermo <ayatacol>
2021-08-17 14:39:33 -0500

    Se suben cambios

---
+++ src/main/java/com/bootcamp/banca/BancaCurrentAccountEnterpriseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.banca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaCurrentAccountEnterpriseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancaCurrentAccountEnterpriseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/banca/BancaCurrentAccountEnterpriseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.banca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancaCurrentAccountEnterpriseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
